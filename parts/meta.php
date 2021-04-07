12d45466f083f6f10f83357da6c8c6af2db1ddc1
akshatsonar <78786935+akshatsonar@users.noreply.github.com>
2021-04-07 16:01:05 +0530

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width">
+
+<base href="http://akshatsonar.co.in/akshatsonar.co.in/public_html/aau/wnm608/sonar.akshat/">
+
+<meta rel="stylesheet" href="css/style.css">
+<meta rel="stylesheet" href="css/product.css">
+<meta rel="stylesheet" href="css/store.css">
---
+++ parts/navbar.php
@@ -0,0 +1,10 @@
+<div class="nav">
+            <nav class="navi">
+                <ul class="navii">
+                    <li><a href="index.php" class="nav1">Home</a></li>
+                    <li><a href="about.php" class="nav1">About</a></li>
+                    <li><a href="store.php" class="nav1">Store</a></li>
+                    <li><a href="contact.php" class="nav1">Contact Us</a></li>
+                </ul>
+            </nav> 
+</div>
